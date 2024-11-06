ec8f916d43e788dcd5979c0192e0e537c5a80db5
Patty <83130286+pattywxf112@users.noreply.github.com>
2024-11-06 23:31:08 +0700

    Update BoardTestNullSquare.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTestNullSquare.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTestNullSquare.java
@@ -5,32 +5,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
     /**
-     this is a comment of boardtestnullsquare
+     this is a comment of boardtestnullsquare.
      */
 public class BoardTestNullSquare { 
 
     private Board boardWithSquare;
     private Board boardWithNullSquare;
-
     private Square square;
-
-        /**
-     this is a comment of boardtestnullsquare
+    /**
+     this is a comment of boardtestnullsquare.
      */
     @BeforeEach 
     void setUp() { 
         square = new BasicSquare();
 
-        // Board with a valid square
+        // Board with a valid square.
         Square[][] gridWithSquare = {{ square }};
         boardWithSquare = new Board(gridWithSquare);
 
-        // Board with a null square
+        // Board with a null square.
         Square[][] gridWithNullSquare = {{ null }};
         boardWithNullSquare = new Board(gridWithNullSquare);
     }
     /**
-     this is a comment of boardtestnullsquare
+     this is a comment of boardtestnullsquare.
      */
     @Test 
     void testSquareAtValidSquare() {
@@ -38,7 +36,7 @@ public class BoardTestNullSquare {
         assertThat(retrievedSquare).isEqualTo(square);
     }
     /**
-     this is a comment of boardtestnullsquare
+     this is a comment of boardtestnullsquare.
      */
     @Test 
     void testSquareAtNullSquare() {
