99b29e8a77fa9dcff9c8015c40bb5fecd2559892
Patty <83130286+pattywxf112@users.noreply.github.com>
2024-11-06 23:29:21 +0700

    Update BoardTestNullSquare.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTestNullSquare.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTestNullSquare.java
@@ -3,16 +3,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//this is a comment
-public class BoardTestNullSquare { //this is a comment of boardtestnullsquare
-//this is a comment
+
+    /**
+     this is a comment of boardtestnullsquare
+     */
+public class BoardTestNullSquare { 
+
     private Board boardWithSquare;
     private Board boardWithNullSquare;
 
     private Square square;
-//this is a comment
-    @BeforeEach //this is beforeach
-    void setUp() { //this is a comment
+
+        /**
+     this is a comment of boardtestnullsquare
+     */
+    @BeforeEach 
+    void setUp() { 
         square = new BasicSquare();
 
         // Board with a valid square
@@ -23,14 +29,18 @@ public class BoardTestNullSquare { //this is a comment of boardtestnullsquare
         Square[][] gridWithNullSquare = {{ null }};
         boardWithNullSquare = new Board(gridWithNullSquare);
     }
-//this is a comment
-    @Test //thisisatest
+    /**
+     this is a comment of boardtestnullsquare
+     */
+    @Test 
     void testSquareAtValidSquare() {
         Square retrievedSquare = boardWithSquare.squareAt(0, 0);
         assertThat(retrievedSquare).isEqualTo(square);
     }
-//this is a comment
-    @Test //thisisatestaswell
+    /**
+     this is a comment of boardtestnullsquare
+     */
+    @Test 
     void testSquareAtNullSquare() {
         assertThrows(AssertionError.class, () -> {
             boardWithNullSquare.squareAt(0, 0);
